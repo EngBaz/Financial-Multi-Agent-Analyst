aa0093acded8bfac65e362ff1d45e1a8c595271f
Roland Baz <94540491+EngBaz@users.noreply.github.com>
2025-01-22 18:53:55 +0100

    update

--- main.py
+++ main.py
@@ -3,63 +3,47 @@ import streamlit as st
 import yfinance as yf
 import pandas as pd
 import time
+import yaml
 
-from crewai import Agent, Task, Crew
-# from langchain_groq import ChatGroq
+from crewai import Agent, Task, Crew, Process
 from crewai_tools import ScrapeWebsiteTool, SerperDevTool, BaseTool
+from langchain_openai import ChatOpenAI
 
 from dotenv import load_dotenv
 load_dotenv()
 
-# Set API keys for the services used (GROQ, Cohere, Google and OpenAI) from environment variables 
 GROQ_API_KEY = os.environ["GROQ_API_KEY"]
 #COHERE_API_KEY = os.environ["COHERE_API_KEY"]
 OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 #GOOGLE_API_KEY = os.environ["GOOGLE_API_KEY"]
 
-# llm = ChatGroq(model="mixtral-8x7b-32768", temperature=0)
-
 search_tool = SerperDevTool()
 scrape_tool = ScrapeWebsiteTool()
 
-#import yfinance as yf
+# Define file paths for YAML files
 
-#ticker = "BTC-USD"
-#period = "1y"
+files = {
+    'data_analyst_agent': 'configs/agents.yaml'
+    }
 
-#btc = yf.Ticker(ticker)
-#historical_data = btc.history(period=period)
-#info = btc.info
+configs = {}
+for config_type, file_path in files.items():
+    with open(file_path, 'r') as file:
+        configs[config_type] = yaml.safe_load(file)
 
-#name = info.get('name', 'N/A')
-#symbol = info.get('symbol', 'N/A')
-#market_cap = info.get('marketCap', 'N/A')
-#circulating_supply = info.get('circulatingSupply', 'N/A') 
-
-#print("Historical Data:")
-#print(historical_data.head())
-
-#print("\nInfo Data:")
-#print(info)
-
-#print("---------")
-#print(name)
-#print(symbol)
-#print(market_cap)
-#print(circulating_supply)
-
+print(configs)
+data_analyst_agent_config = configs['data_analyst_agent']
 
 class CryptoInfoTool(BaseTool):
     name: str = "get_basic_crypto_info"
     description: str = "Fetch basic crypto information for a given ticker and period."
 
-    def _run(self, ticker: str, period: str = '1y') -> str:
+    def _run(self, ticker: str, period: str) -> str:
         try:
             btc = yf.Ticker(ticker)
             historical_data = btc.history(period=period)
             info = btc.info
 
-            # Extract values with default fallbacks
             name = info.get('name', 'N/A')
             symbol = info.get('symbol', 'N/A')
             market_cap = info.get('marketCap', 'N/A')
@@ -69,7 +53,6 @@ class CryptoInfoTool(BaseTool):
             stock_max = round(historical_data['Close'].max(), 2) if not historical_data.empty else 'N/A'
             stock_min = round(historical_data['Close'].min(), 2) if not historical_data.empty else 'N/A'
 
-            # Construct the response
             response = f"""
             **Cryptocurrency Analysis:**
             - Name: {name}
@@ -90,13 +73,9 @@ crypto_info_tool = CryptoInfoTool()
 
 print(crypto_info_tool)
 
-crypto_researcher = Agent(
-    role="Cryptocurrency Researcher",
-    goal="Gather and display detailed financial metrics and performance data for a given cryptocurrency, including its name, ticker, market cap, circulating supply, and historical stock price metrics over a specified period.",
-    backstory="An experienced cryptocurrency researcher dedicated to providing concise, actionable insights about cryptocurrency performance and key financial metrics.",
+data_analyst = Agent(
+    config=data_analyst_agent_config["data_analyst_agent"],
     tools=[crypto_info_tool],
-    verbose=True,
-    allow_delegation=False,
 )
 
 collect_crypto_info = Task(
@@ -105,7 +84,8 @@ collect_crypto_info = Task(
     2. If no ticker or timeframe is mentioned, identify the cryptocurrency and use a default timeframe of 1 year.
     3. Provide a clear and concise summary of the financial metrics and performance data gathered.
 
-    User query: {query}.
+    User crypto: {ticker}.
+    User period: {period}.
 
     Your response should include:
     - Cryptocurrency Name and Ticker
@@ -115,14 +95,17 @@ collect_crypto_info = Task(
     - Any relevant additional insights based on the query.
     ''',
     expected_output="A detailed report summarizing the cryptocurrency's financial metrics and historical performance.",
-    agent=crypto_researcher,
+    agent=data_analyst,
     dependencies=[],
     context=[]
 )
 
 crew = Crew(
-    agents=[crypto_researcher],
-    tasks=[collect_crypto_info]
+    agents=[data_analyst],
+    tasks=[collect_crypto_info],
+    process=Process.hierarchical,
+    verbose=True,
+    manager_llm=ChatOpenAI(model="gpt-4-turbo", temperature=0.7),
 )
 
 def stream_data(response):
@@ -130,19 +113,24 @@ def stream_data(response):
     for word in response.split(" "):
         yield word + " "
         time.sleep(0.06)
-
-## Streamlit application Configuration      
+     
 st.set_page_config(page_title="Advanced Cryptocurrency Analysis Dashboard", layout="wide")
 
 st.title("Advanced Cryptocurrency Analysis Dashboard")
 
-st.sidebar.header("Cryptocurrency Analysis Query")
-query = st.sidebar.text_area("Enter your cryptocurrency analysis question", 
-                             value="Is Bitcoin a safe long-term bet for a risk-averse individual?", height=100)
+st.sidebar.header("Cryptocurrency Analysis Input")
+
+crypto_tickers = ["BTC-USD", "ETH-USD", "ADA-USD", "XRP-USD"]
+selected_crypto_ticker = st.sidebar.selectbox("Select a crypto:", crypto_tickers)
+
+period = ["1y", "2y", "3y"]
+selected_period = st.sidebar.selectbox("Select a period:", period)
+
 analyze_button = st.sidebar.button("Analyze")
 
 if analyze_button:
     st.info(f"Starting analysis. This may take a few minutes...")
-    result = crew.kickoff(inputs={"query": query})
+    result = crew.kickoff(inputs={"ticker": selected_crypto_ticker,
+                                  "period": selected_period})
     st.success("Analysis complete!")
     st.markdown(result)
